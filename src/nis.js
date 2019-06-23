ce36703332cc33febade3a18017fec59301007ed
Troy Do <do@infinitylab.world>
2019-06-23 17:26:25 -0500

    First Sketch Plugin

---
+++ src/nis.js
@@ -0,0 +1,14 @@
+var sketch = require('sketch/dom')
+var async = require('sketch/async')
+var DataSupplier = require('sketch/data-supplier')
+var UI = require('sketch/ui')
+var Settings = require('sketch/settings')
+
+
+// Install App on Sketch
+
+// Getting Input from User
+
+// Make API Call to Image API
+
+// Display Image on Selected Canvas
